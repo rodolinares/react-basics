8d36aec46d151f3e1fa30589f3621275ea0fb8a0
Rodrigo Linares <rodolinares19@gmail.com>
2023-08-30 18:59:09 -0500

    Create home component

--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../../logo.svg";
 
-function App() {
+const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
